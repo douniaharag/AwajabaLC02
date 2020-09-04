9b1852d8e059d950e9d5e858101c555f41ffd5ae
douniaharag <douniaharagsio@gmail.com>
2020-09-04 15:40:50 +0200

    commit#3

--- Repas.java
+++ Repas.java
@@ -7,5 +7,9 @@ public class Repas {
     private int numero;
     private LocalDate date;
   
-
+public Repas(int numero,  LocalDate date) {
+		this.numero = numero
+        this.dateInscription = dateInscription;
+      
+    }
      
